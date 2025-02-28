b99e9b654eeebbfbb42e2dc78fbc936b0610f79f
Nguyễn Kiêm Mạnh <152244027+lionelmahn@users.noreply.github.com>
2025-02-28 15:08:48 +0000

    update layout

--- qlhocvien/resources/views/auth/forgot-password.blade.php
+++ qlhocvien/resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Quên mật khẩu của bạn? Không có gì. Chỉ cần cho chúng tôi biết địa chỉ email của bạn và chúng tôi sẽ gửi email cho bạn một liên kết đặt lại mật khẩu cho phép bạn chọn một địa chỉ mới.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Liên kết đặt lại mật khẩu email') }}
             </x-primary-button>
         </div>
     </form>
--- qlhocvien/resources/views/layouts/navigation.blade.php
+++ qlhocvien/resources/views/layouts/navigation.blade.php
@@ -81,7 +81,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Thông tin tài khoản') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
@@ -91,7 +91,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Đăng xuất') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
